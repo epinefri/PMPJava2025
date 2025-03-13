b7b636d5db70aaa4d9ec09ae760e5a19968caed7
epinefri <danielasorinap@gmail.com>
2025-03-13 11:47:58 +0200

    Adaugare notite pt input date

--- src/ProgramHello.java
+++ src/ProgramHello.java
@@ -1,5 +1,22 @@
+import java.util.Scanner; // importa clasa Scanner
+
 public class ProgramHello {
     public static void main(String[] args){
-        System.out.print("Hello!");
+        System.out.println("Hello!");
+
+        // creare obiect Scanner:
+
+            // Scanner (clasa) numeleTau =
+            // = new (operator care aloca spatiu in mem) Scanner (constructor)
+            // (System.in - flux intrare date tastatura)
+        Scanner scannerulMeu = new Scanner(System.in);
+
+        System.out.print("Nume: ");
+
+        // metoda pt cerere input tastatura
+            // nextLine() - citeste o linie completa de text de la tastatura
+        String numeleTau = scannerulMeu.nextLine();
+
+        System.out.println("Salut, " + numeleTau +"!");
     }
 }
