d74913adab03ae286ae8510bac6121480eea2185
epinefri <danielasorinap@gmail.com>
2025-03-14 11:21:09 +0200

    Adaugare metoda afisare

--- src/ProgramMasiveUnidimensionale.java
+++ src/ProgramMasiveUnidimensionale.java
@@ -1,6 +1,5 @@
 /**
  * Program Masive unidimensionale
- *
  * - citire parametri din linia de comandă
  * - utilizare funcții din clasa String
  * - utilizare funcții de conversie din clase wrapper
@@ -10,16 +9,26 @@
 //import java.util.Scanner;
 
 public class ProgramMasiveUnidimensionale {
+    // metoda pt afisare mesaj si elem vector
+    static void afisare(String mesaj, int[] vector){
+        System.out.printf(mesaj + ": ");
+        for(int i = 0; i < vector.length; i++)
+            System.out.print(vector[i] + " ");
+        System.out.println();
+    }
+
     public static void main(String[] args){
         // citire parametri din linia de comandă / un singur arg
         String[] valoriString = args[0].split(",");// split aloca si spatiul necesar
 
         // convertire valori din vectorul de strings -> vector int
-        Integer[] valori = new Integer[valoriString.length];
+        int[] valori = new int[valoriString.length];
         for (int i = 0; i < valoriString.length; i++){
+            // elimina spatii goale + convertire fiecare element la int
             valori[i] = Integer.parseInt(valoriString[i].trim());
-            System.out.println(valori[i]);
         }
+
+        afisare("elemente", valori);
 
 
     }
